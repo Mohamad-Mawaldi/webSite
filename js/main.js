70e742827ac4fb7f04f3268d5fac89bf1eb27931
Mohamad Mawaldi <ima.mawaldi@gmail.com>
2018-08-21 22:13:52 +0200

    work section is done.
    added magnific Popup plugin and the zome in theme for the galery.

--- js/main.js
+++ js/main.js
@@ -4,13 +4,19 @@
 $(function () {
     // animate on scroll
     new WOW().init();
-
-
-
-
-
-
-
-
-
 })
+
+/*============================================
+                  WORK
+=============================================*/
+$(function () {
+            // magnificPopup
+            $('#work').magnificPopup({
+                    delegate: 'a', // child items selector, by clicking on it popup will open
+                    type: 'image'
+                    gallery: {
+                        enabled: true
+
+
+                    });
+            });
