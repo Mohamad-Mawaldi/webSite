4d63665716ce5c38b2aa2d8a2097edb6e615f076
Mohamad Mawaldi <ima.mawaldi@gmail.com>
2018-08-22 10:35:30 +0200

    fixed the galery, added the html markup for the team section.

--- js/main.js
+++ js/main.js
@@ -2,21 +2,19 @@
                   SERVICE
 =============================================*/
 $(function () {
-    // animate on scroll
     new WOW().init();
+   
 })
 
 /*============================================
                   WORK
 =============================================*/
 $(function () {
-            // magnificPopup
-            $('#work').magnificPopup({
-                    delegate: 'a', // child items selector, by clicking on it popup will open
-                    type: 'image'
-                    gallery: {
-                        enabled: true
-
-
-                    });
-            });
+    $('#work').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+})
