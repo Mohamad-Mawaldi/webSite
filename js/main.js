84b2d5dc91083a6c078a4ede45b20265ce1b5bd7
Mohamad Mawaldi <ima.mawaldi@gmail.com>
2018-08-23 23:30:00 +0200

    client section is done.

--- js/main.js
+++ js/main.js
@@ -54,3 +54,15 @@ $(function () {
         time: 1500
     });
 });
+/*============================================
+                  Clients
+=============================================*/
+$(function () {
+    $("#clients-list").owlCarousel({
+        items: 6,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+})
