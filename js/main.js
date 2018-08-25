f1a1805a806e0808139dc50c098bdf5230aa3334
Mohamad Mawaldi <ima.mawaldi@gmail.com>
2018-08-25 23:15:26 +0200

    added the smooth-scroll and scroll-spy.

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ $(function () {
         delay: 10,
         time: 1500
     });
-});
+})
 /*============================================
                   Clients
 =============================================*/
@@ -64,5 +64,39 @@ $(function () {
         smartSpeed: 700,
         loop: true,
         autoplayHoverPause: true
+    });
+})
+/*============================================
+                  Navigation
+=============================================*/
+$(function () {
+
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() < 50) {
+            //hide nav 
+            $("nav").removeClass("website-background");
+        } else {
+            //show nav
+            $("nav").addClass("website-background");
+        }
+    });
+})
+
+
+/*============================================
+                  smooth scrolling
+=============================================*/
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+
+        event.preventDefault();
+        var section = $(this).attr("href");
+
+        $("html, body").animate({
+            scrollTop: $(section).offset().top - 64
+        }, 1250, "easeInOutExpo");
     });
 })
